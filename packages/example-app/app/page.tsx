a14d0178148a58746de99181a5b60eaeda33260a
Liboul <matthieulibeer@gmail.com>
2025-02-04 16:43:31 +0100

    Add UI package and use it in example-app

--- packages/example-app/app/page.tsx
+++ packages/example-app/app/page.tsx
@@ -1,7 +1,12 @@
+import { Ui } from '@startup-template/ui';
+
 export default function Index() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <h2 className="text-2xl font-bold">Component from ui package</h2>
+      <Ui />
     </>
   );
 }
---
+++ packages/ui/README.md
@@ -0,0 +1,7 @@
+# @startup-template/ui
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @startup-template/ui` to execute the unit tests via [Jest](https://jestjs.io).
