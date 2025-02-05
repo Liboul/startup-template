c5d7ba987eb13e4df559a0091efbfb411b887552
Liboul <matthieulibeer@gmail.com>
2025-02-05 02:56:40 +0100

    Add DB call to notebook

--- packages/notebook/index.ts
+++ packages/notebook/index.ts
@@ -1,7 +1,17 @@
 import { exampleFunctionFromLib } from '@startup-template/example-lib';
+import { prisma } from '@startup-template/db';
+async function testFunctionFromLib() {
+  console.log(exampleFunctionFromLib());
+}
+
+async function testDbCall() {
+  const posts = await prisma.examplePost.findMany();
+  console.log(`posts: ${JSON.stringify(posts, null, 2)}`);
+}
 
 async function main() {
-  console.log(exampleFunctionFromLib());
+  await testFunctionFromLib();
+  await testDbCall();
 }
 
 main();
