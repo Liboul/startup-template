e57e138282da36b3bdc721e550d60def0d1c6de4
Liboul <matthieulibeer@gmail.com>
2025-02-05 01:08:25 +0100

    Add dependency on recruiter-app dev with DB

--- packages/db/start.sh
+++ packages/db/start.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 # Use this script to start a docker container for a local development database
 
-# TO RUN ON WINDOWS:
-# 1. Install WSL (Windows Subsystem for Linux) - https://learn.microsoft.com/en-us/windows/wsl/install
-# 2. Install Docker Desktop for Windows - https://docs.docker.com/docker-for-windows/install/
-# 3. Open WSL - `wsl`
-# 4. Run this script - `./start-database.sh`
-
-# On Linux and macOS you can run this script directly - `./start-database.sh`
-
 # import env variables from .env
 set -a
 source .env
@@ -42,10 +34,11 @@ if [ "$(docker ps -q -a -f name=$DB_CONTAINER_NAME)" ]; then
   exit 0
 fi
 
-docker run -d \
-  --name $DB_CONTAINER_NAME \
-  -e POSTGRES_USER=$DB_USER \
+docker run \
+  --name "$DB_CONTAINER_NAME" \
+  -e POSTGRES_USER="$DB_USER" \
   -e POSTGRES_PASSWORD="$DB_PASSWORD" \
-  -e POSTGRES_DB=$DB_NAME \
+  -e POSTGRES_DB="$DB_NAME" \
   -p "$DB_PORT":5432 \
+  -v "$(pwd)/data/postgres:/var/lib/postgresql/data" \
   docker.io/postgres && echo "Database container '$DB_CONTAINER_NAME' was successfully created"
