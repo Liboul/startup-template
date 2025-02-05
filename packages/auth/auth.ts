bbf7e5b6b3a9ceb1472b4664dc2abd75bfdbe440
Liboul <matthieulibeer@gmail.com>
2025-02-05 06:36:44 +0100

    Integrate better auth with next

--- packages/auth/auth.ts
+++ packages/auth/auth.ts
@@ -2,8 +2,25 @@ import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { db } from '@startup-template/db';
 
+if (!process.env.GOOGLE_CLIENT_ID) {
+  throw new Error('GOOGLE_CLIENT_ID is not set');
+}
+
+if (!process.env.GOOGLE_CLIENT_SECRET) {
+  throw new Error('GOOGLE_CLIENT_SECRET is not set');
+}
+
 export const auth = betterAuth({
   database: prismaAdapter(db, {
     provider: 'postgresql',
   }),
+  emailAndPassword: {
+    enabled: false,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+  },
 });
